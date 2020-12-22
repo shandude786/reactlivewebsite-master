a6b13903a7c80433fe2c86d60161a648666eabe1
SHANTI LAL <shandude786@users.noreply.github.com>
2020-12-22 10:19:39 +0530

    this is second commit

---
+++ build/precache-manifest.8a3670cd1d97c6029a26e6f9f8c47cb8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b119ed7886e40cfaa9d915ffddee79b0",
+    "url": "/reactlivewebsite-master/index.html"
+  },
+  {
+    "revision": "b0d04d0ae6f22409805f",
+    "url": "/reactlivewebsite-master/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "e6dbeb141e58f116b3e2",
+    "url": "/reactlivewebsite-master/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "b0d04d0ae6f22409805f",
+    "url": "/reactlivewebsite-master/static/js/2.604bbc28.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactlivewebsite-master/static/js/2.604bbc28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6dbeb141e58f116b3e2",
+    "url": "/reactlivewebsite-master/static/js/main.3e51c169.chunk.js"
+  },
+  {
+    "revision": "c7eb21c5a464ca1d91ed",
+    "url": "/reactlivewebsite-master/static/js/runtime-main.67f74c7d.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/reactlivewebsite-master/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/reactlivewebsite-master/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/reactlivewebsite-master/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/reactlivewebsite-master/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/reactlivewebsite-master/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/reactlivewebsite-master/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/reactlivewebsite-master/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/reactlivewebsite-master/static/media/software.43049cae.jpeg"
+  }
+]);
